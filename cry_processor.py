6b7d7982d082ca5a4daee32376f4ffa6c0c4345c
anton-shikov <shik-999@inbox.ru>
2019-03-26 00:21:39 +0300

    adding diamond annotation

--- cry_processor.py
+++ cry_processor.py
@@ -1,6 +1,6 @@
 import subprocess
 import argparse
-from Bio import SeqIO
+from Bio import SeqIO, Entrez
 from Bio.Seq import Seq
 from Bio.Alphabet import generic_protein
 from Bio.SeqRecord import SeqRecord
@@ -8,6 +8,8 @@ from Bio.Blast import NCBIWWW, NCBIXML
 from collections import defaultdict
 import sys
 import csv
+import time
+
 
 
 class CryProcessor:
@@ -81,43 +83,25 @@ class CryProcessor:
         print('{} toxins with two domains'.format(self.two_dom_count))
         print('{} toxins with three domains'.format(self.three_dom_count))
 
-    def blast_raw_output(self):
-        id_match_table = defaultdict(list)
-        new_ids=list()
-        anotated_rec_list=list()
-        print("Blasting raw output")   
-        result_handle = NCBIWWW.qblast("blastp","nr",open('cry_extraction/raw_processed_{}.fasta'.format(str(self.cry_quiery).split('.')[0])).read(),megablast=False,expect=1000,word_size=6,hitlist_size=10,threshold=21)
-        blast_stat = open('cry_extraction/logs/blast_alignments_{}.log'.format(str(self.cry_quiery).split('.')[0]),'w')
-        for record in NCBIXML.parse(result_handle):
-            blast_stat.write('********************************************************\n')
-            if record.alignments:
-                for alignment in record.alignments:
-                    print >> blast_stat,alignment
-                    blast_stat.write('\n')
-                    for hsp in alignment.hsps:
-                        print >> blast_stat,hsp
-                        blast_stat.write('\n')
-            #filtering first hit base on bit_score
-                record.alignments.sort(key = lambda align: -max(hsp.score for hsp in align.hsps))
-                new_ids.append([str(record.alignments[0].hit_id.split('|')[3]).strip(), str(record.alignments[0].hit_id).strip()]) 
-            else:
-                new_ids.append(["NO BLAST MATCH", '-']) 
-        blast_stat.close()
-        for init_rec, new_rec in zip(SeqIO.parse(open('cry_extraction/raw_processed_{}.fasta'.format(str(self.cry_quiery).split('.')[0])),"fasta"),new_ids):
-            id_match_table[init_rec.id].append(init_rec.description)
-            id_match_table[init_rec.id].extend(new_rec)
-            anotated_rec_list.append(SeqRecord(Seq(str(init_rec.seq),generic_protein),id=new_rec[0]))
-        SeqIO.write(anotated_rec_list,'cry_extraction/raw_blasted_{}.fasta'.format(str(self.cry_quiery).split('.')[0]), "fasta")
-        csvwfile = open('cry_extraction/logs/id_match_table_{}.tsv'.format(str(self.cry_quiery).split('.')[0]), 'w')
-        my_writer = csv.writer(csvwfile, delimiter='\t')
-        my_writer.writerow(['Initial ID', 'Description', 'Blast hit ID', 'Description'])
-        print(len(new_ids))
-        print(len(id_match_table))
-        for init_id in id_match_table:
-            my_writer.writerow([init_id, id_match_table[init_id][0], id_match_table[init_id][1],id_match_table[init_id][2]])
-        csvwfile.close()
-          
-
+    def annotate_raw_output(self):
+        new_records = list()
+        new_ids=dict()
+        un_count=0
+        print("Annotating raw output with diamond")  
+        cmd_pre_dia = subprocess.call('cd cry_extraction; cp ../data/diamond_data/cry_nomenclature.dmnd .; touch diamond.log', shell=True) 
+        cmd_dia = subprocess.call('cd cry_extraction;../diamond blastp -d cry_nomenclature -q raw_processed_{0}.fasta -o diamond_matches_{0}.txt --al aligned_{0}.fa --un unaligned_{0}.fa --max-target-seqs 1 --log --verbose 2>> diamond.log; rm cry_nomenclature.dmnd; mv *.log logs/; mv *gned* logs/'.format(str(self.cry_quiery).split('.')[0]), shell=True)
+        with open("cry_extraction/diamond_matches_{}.txt".format(str(self.cry_quiery).split('.')[0]), 'r') as csv_file:
+            my_reader = csv.reader(csv_file, delimiter='\t') 
+            for row in my_reader:
+                if float(row[2])<100.0:
+                    new_ids[row[0]]=row[1]+'|'+ str(row[2])
+                    un_count+=1
+        for init_rec in SeqIO.parse(open('cry_extraction/raw_processed_{}.fasta'.format(str(self.cry_quiery).split('.')[0])),"fasta"):
+           if init_rec.id in new_ids.keys():
+               new_records.append(SeqRecord(Seq(str(init_rec.seq),generic_protein),id=new_ids[init_rec.id],description=init_rec.description))
+        print('{} different from database toxins found'.format(un_count))
+        SeqIO.write(new_records,'cry_extraction/unique_{}.fasta'.format(str(self.cry_quiery).split('.')[0]), "fasta")
+       
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='cry_processor')
     parser.add_argument('-fi', help='Please enter full path to fasta file', metavar='File',
@@ -129,7 +113,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
     fi,hm,pr = args.fi, args.hm, args.pr
     pr = CryProcessor(fi, hm,pr)
-    pr.find_cry()
-    pr.find_domains()
+    #pr.find_cry()
+    #pr.find_domains()
     pr.cry_digestor()
-    pr.blast_raw_output()
+    pr.annotate_raw_output()
